2a4c8903a1a1fe4dc100e67b1ab0385aab848ea7
vadim <vadimradyno@gmail.com>
2014-05-04 01:30:31 +0300

    - task 4

---
+++ solutions/vadim_radyno/4/sources/4_5/solution.cpp
@@ -0,0 +1,63 @@
+#include "solution.h"
+
+task4_5::solution::solution( const data_type& data )
+    : m_data(data)
+    , m_max(getFirstValueFromData())
+    , m_min(m_max)
+{
+    boost::thread_group group_of_slave_threads;
+    int counter_thread = ms_count_thread;
+    while(counter_thread--)
+    {
+        group_of_slave_threads.create_thread(boost::bind( &solution::findMinMax, this));
+    }
+
+    group_of_slave_threads.join_all();
+}
+
+int task4_5::solution::get_min() const
+{
+    return m_min;
+}
+int task4_5::solution::get_max() const
+{
+    return m_max;
+}
+
+
+int task4_5::solution::getFirstValueFromData() const
+{
+    for (auto data_elem : m_data)
+    {
+        for (int current_value : data_elem)
+        {
+            return current_value;
+        }
+    }
+
+    return 0;
+}
+
+
+void task4_5::solution::findMinMax()
+{
+    while (true)
+    {
+        boost::mutex::scoped_lock lock_data(m_wait_data);
+        if (m_data.empty())
+        {
+            return;
+        }
+
+        auto data = m_data.back();
+        m_data.pop_back();
+        lock_data.unlock();
+
+        boost::mutex::scoped_lock lock_min_and_max(m_wait_min_max);
+        for (const int current_value : data)
+        {
+            m_min = std::min(current_value, m_min);
+            m_max = std::max(current_value, m_max);
+        }
+    }
+}
